1fe60993d2a51238ba03eab2b467d43278fce77a
Sanjay <sanjaym0919@gmail.com>
2024-04-05 11:54:58 +0530

    PostView Component added

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,6 +1,17 @@
+import { clerkClient } from "@clerk/nextjs";
+import type { User } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    imageUrl: user.imageUrl,
+  };
+}
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
@@ -12,7 +23,7 @@ export const postRouter = createTRPCRouter({
     }),
 
   create: publicProcedure
-    .input(z.object({ name: z.string().min(1), id:z.number(), createdAt: z.date(), updatedAt: z.date() , content: z.string().max(255), authorID: z.string()}))
+    .input(z.object({ name: z.string().min(1), id: z.number(), createdAt: z.date(), updatedAt: z.date(), content: z.string().max(255), authorID: z.string() }))
     .mutation(async ({ ctx, input }) => {
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
@@ -20,10 +31,10 @@ export const postRouter = createTRPCRouter({
       return ctx.db.post.create({
         data: {
           name: input.name,
-          id: input.id, 
+          id: input.id,
           createdAt: input.createdAt,
           updatedAt: input.updatedAt,
-          content: input.content ,
+          content: input.content,
           authorID: input.authorID,
         },
       });
@@ -34,7 +45,32 @@ export const postRouter = createTRPCRouter({
       orderBy: { createdAt: "desc" },
     });
   }),
-  getAll: publicProcedure.query(({ctx})=> {
-    return ctx.db.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+    });
+
+    const users = (await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorID),
+      limit: 100,
+    })).map(filterUserForClient);
+
+    console.log(users);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorID);
+      if(!author || !author.username) throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Author for post not found"
+      })
+
+      return {
+        post,
+        author:{
+          ...author,
+          username: author.username,
+        }
+      }
+    });
   })
 });
