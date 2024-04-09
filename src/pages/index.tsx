f3c6fc81baf4ac19014b205754bce1717e8dea5e
Sanjay <sanjaym0919@gmail.com>
2024-04-09 11:34:45 +0530

    another possible fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Head from "next/head";
 import Link from "next/link";
 import Loading, { LoadingSpinner } from "~/components/Loading";
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -2,6 +2,8 @@ import { clerkClient } from "@clerk/nextjs";
 import type { User } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+import { Ratelimit } from "@upstash/ratelimit"
+import { Redis } from "@upstash/redis"
 
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 
@@ -12,13 +14,7 @@ const filterUserForClient = (user: User) => {
     imageUrl: user.imageUrl,
   };
 }
-import  {Ratelimit} from "@upstash/ratelimit"
-import {Redis} from "@upstash/redis"
-const rateLimit = new Ratelimit ( {
-  redis:Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(3, "1 m"),
-  analytics: true,
-});
+
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
@@ -56,7 +52,7 @@ export const postRouter = createTRPCRouter({
     const posts = await ctx.db.post.findMany({
       take: 100,
       orderBy: [
-        {createdAt:"desc"}
+        { createdAt: "desc" }
       ]
     });
 
@@ -86,23 +82,29 @@ export const postRouter = createTRPCRouter({
   create: privateProcedure.input(
     z.object(
       {
-        content:z.string().emoji().min(1).max(200),
+        content: z.string().emoji().min(1).max(200),
         name: z.string(),
       }
     )
-  ).mutation(async ({ctx, input}) => {
-      const authorID = String(ctx.currentUserID);
+  ).mutation(async ({ ctx, input }) => {
+    const rateLimit = new Ratelimit({
+      redis: Redis.fromEnv(),
+      limiter: Ratelimit.slidingWindow(3, "1 m"),
+      analytics: true,
+    });
+
+    const authorID = String(ctx.currentUserID);
 
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    const {success} = await rateLimit.limit(authorID);
+    const { success } = await rateLimit.limit(authorID);
 
-    if(!success) throw new TRPCError({code: "TOO_MANY_REQUESTS" })
+    if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" })
 
     const post = ctx.db.post.create({
       data: {
         authorID,
-        name:input.name,
+        name: input.name,
         content: input.content,
       },
     });
